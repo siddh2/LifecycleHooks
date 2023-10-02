be1c6a8b7e0fca0a1e3d16ada80ceed2eeab37da
Siddharth Gautam <68075023+siddh2@users.noreply.github.com>
2023-10-02 11:40:52 +0530

    Add files via upload

---
+++ lifeCycleChild/lifeCycleChild.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+
+export default class LifeCycleChild extends LightningElement {
+
+    constructor(){
+        super()
+            console.log("child constructor called")
+        
+    }
+    connectedCallback(){
+        console.log("child connectedCallback called")
+    }
+    renderedCallback(){
+        console.log("child renderedCallback called")
+    }
+    disconnectedCallback(){
+        alert('Child disconnectedCallback called !!')
+    }
+}
